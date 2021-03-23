e953b8425bf826140207f0e470712f8d120c4324
Tim Hansen <tihansen@redhat.com>
2021-03-23 12:40:46 -0400

    Remove logic for other platforms besides linux.
    Signed-off-by: Tim Hansen <tihansen@redhat.com>

--- prepare_vscode.sh
+++ prepare_vscode.sh
@@ -11,12 +11,7 @@ cd vscode || exit
 patch -u src/vs/platform/update/electron-main/updateService.win32.ts -i ../patches/update-cache-path.patch
 patch -u resources/linux/rpm/code.spec.template -i ../patches/fix-rpm-spec.patch
 
-if [[ "$OS_NAME" == "osx" ]]; then
-  CHILD_CONCURRENCY=1 yarn --frozen-lockfile --ignore-optional
-  npm_config_argv='{"original":["--ignore-optional"]}' yarn postinstall
-else
-  CHILD_CONCURRENCY=1 yarn --frozen-lockfile
-fi
+CHILD_CONCURRENCY=1 yarn --frozen-lockfile
 
 mv product.json product.json.bak
 
@@ -57,35 +52,33 @@ cat product.json
 
 ../undo_telemetry.sh
 
-if [[ "$OS_NAME" == "linux" ]]; then
-  # microsoft adds their apt repo to sources
-  # unless the app name is code-oss
-  # as we are renaming the application to vscodium
-  # we need to edit a line in the post install template
-  sed -i "s/code-oss/codium/" resources/linux/debian/postinst.template
-  
-  # fix the packages metadata
-  # code.appdata.xml
-  sed -i 's|Visual Studio Code|VSCodium|g' resources/linux/code.appdata.xml
-  sed -i 's|https://code.visualstudio.com/docs/setup/linux|https://github.com/VSCodium/vscodium#download-install|' resources/linux/code.appdata.xml
-  sed -i 's|https://code.visualstudio.com/home/home-screenshot-linux-lg.png|https://vscodium.com/img/vscodium.png|' resources/linux/code.appdata.xml
-  sed -i 's|https://code.visualstudio.com|https://vscodium.com|' resources/linux/code.appdata.xml
-  
-  # control.template
-  sed -i 's|Microsoft Corporation <vscode-linux@microsoft.com>|VSCodium Team https://github.com/VSCodium/vscodium/graphs/contributors|'  resources/linux/debian/control.template
-  sed -i 's|https://code.visualstudio.com|https://vscodium.com|' resources/linux/debian/control.template
-  sed -i 's|Visual Studio Code|VSCodium|g' resources/linux/debian/control.template
-  sed -i 's|https://code.visualstudio.com/docs/setup/linux|https://github.com/VSCodium/vscodium#download-install|' resources/linux/debian/control.template
-  
-  # code.spec.template
-  sed -i 's|https://code.visualstudio.com/docs/setup/linux|https://github.com/VSCodium/vscodium#download-install|' resources/linux/rpm/code.spec.template
-  sed -i 's|Microsoft Corporation|VSCodium Team|' resources/linux/rpm/code.spec.template
-  sed -i 's|Visual Studio Code Team <vscode-linux@microsoft.com>|VSCodium Team https://github.com/VSCodium/vscodium/graphs/contributors|' resources/linux/rpm/code.spec.template
-  sed -i 's|https://code.visualstudio.com|https://vscodium.com|' resources/linux/rpm/code.spec.template
-  sed -i 's|Visual Studio Code|VSCodium|' resources/linux/rpm/code.spec.template
-  
-  # snapcraft.yaml
-  sed -i 's|Visual Studio Code|VSCodium|'  resources/linux/rpm/code.spec.template
-fi
+# microsoft adds their apt repo to sources
+# unless the app name is code-oss
+# as we are renaming the application to vscodium
+# we need to edit a line in the post install template
+sed -i "s/code-oss/codium/" resources/linux/debian/postinst.template
+
+# fix the packages metadata
+# code.appdata.xml
+sed -i 's|Visual Studio Code|VSCodium|g' resources/linux/code.appdata.xml
+sed -i 's|https://code.visualstudio.com/docs/setup/linux|https://github.com/VSCodium/vscodium#download-install|' resources/linux/code.appdata.xml
+sed -i 's|https://code.visualstudio.com/home/home-screenshot-linux-lg.png|https://vscodium.com/img/vscodium.png|' resources/linux/code.appdata.xml
+sed -i 's|https://code.visualstudio.com|https://vscodium.com|' resources/linux/code.appdata.xml
+
+# control.template
+sed -i 's|Microsoft Corporation <vscode-linux@microsoft.com>|VSCodium Team https://github.com/VSCodium/vscodium/graphs/contributors|'  resources/linux/debian/control.template
+sed -i 's|https://code.visualstudio.com|https://vscodium.com|' resources/linux/debian/control.template
+sed -i 's|Visual Studio Code|VSCodium|g' resources/linux/debian/control.template
+sed -i 's|https://code.visualstudio.com/docs/setup/linux|https://github.com/VSCodium/vscodium#download-install|' resources/linux/debian/control.template
+
+# code.spec.template
+sed -i 's|https://code.visualstudio.com/docs/setup/linux|https://github.com/VSCodium/vscodium#download-install|' resources/linux/rpm/code.spec.template
+sed -i 's|Microsoft Corporation|VSCodium Team|' resources/linux/rpm/code.spec.template
+sed -i 's|Visual Studio Code Team <vscode-linux@microsoft.com>|VSCodium Team https://github.com/VSCodium/vscodium/graphs/contributors|' resources/linux/rpm/code.spec.template
+sed -i 's|https://code.visualstudio.com|https://vscodium.com|' resources/linux/rpm/code.spec.template
+sed -i 's|Visual Studio Code|VSCodium|' resources/linux/rpm/code.spec.template
+
+# snapcraft.yaml
+sed -i 's|Visual Studio Code|VSCodium|'  resources/linux/rpm/code.spec.template
 
 cd ..
