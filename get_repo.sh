7c273438625c626ad66ca7c65df9e42c67294dc5
Tim Hansen <tihansen@redhat.com>
2021-03-23 12:37:53 -0400

    Remove github actions logic
    Signed-off-by: Tim Hansen <tihansen@redhat.com>

--- get_repo.sh
+++ get_repo.sh
@@ -8,8 +8,3 @@ echo "Got the latest MS tag: ${LATEST_MS_TAG}"
 
 git clone https://github.com/Microsoft/vscode.git --branch $LATEST_MS_TAG --depth 1
 
-# for GH actions
-if [[ $GITHUB_ENV ]]; then
-	echo "LATEST_MS_COMMIT=$LATEST_MS_COMMIT" >> $GITHUB_ENV
-	echo "LATEST_MS_TAG=$LATEST_MS_TAG" >> $GITHUB_ENV
-fi
